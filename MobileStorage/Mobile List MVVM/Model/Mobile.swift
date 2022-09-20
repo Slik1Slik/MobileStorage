1ce6a71f6cb22e3e74f752047c3ff51857d07862
Slik <chesto.newsc@ya.ru>
2022-09-20 23:30:19 +0300

    MobileList MVVM: Add model

---
+++ MobileStorage/Mobile List MVVM/Model/Mobile.swift
@@ -0,0 +1,11 @@
+//
+//  Mobile.swift
+//  MobileStorage
+//
+//  Created by Slik on 20.09.2022.
+//
+
+struct MobileModelRepresentation : Hashable {
+    var title: String
+    var subtitle: String
+}
