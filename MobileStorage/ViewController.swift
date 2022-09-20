96f1cb6dfdc299b1ca167362d91f289817fd50e4
Slik <chesto.newsc@ya.ru>
2022-09-20 19:23:09 +0300

    Initial Commit

---
+++ MobileStorage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MobileStorage
+//
+//  Created by Slik on 20.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
