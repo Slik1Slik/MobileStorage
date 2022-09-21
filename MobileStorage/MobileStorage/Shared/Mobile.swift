c5d9bc74fd97a5e5c9730cd731d1404be14bdec2
Slik <chesto.newsc@ya.ru>
2022-09-21 21:18:33 +0300

    Add Mobile struct

---
+++ MobileStorage/MobileStorage/Shared/Mobile.swift
@@ -0,0 +1,13 @@
+//
+//  Mobile.swift
+//  MobileStorage
+//
+//  Created by Slik on 21.09.2022.
+//
+
+import Foundation
+
+struct Mobile: Hashable {
+    let imei: String
+    let model: String
+}
