e98677918fa3a35ecb3dd49761b35d0408cdb84b
Slik <chesto.newsc@ya.ru>
2022-09-26 16:59:27 +0300

    Update CoreDataStorageManager

--- MobileStorage/CoreData/StorageManager.swift
+++ MobileStorage/CoreData/StorageManager.swift
@@ -8,57 +8,66 @@
 import CoreData
 
 final class CoreDataStorageManager<T : NSManagedObject> {
-
+    
     static func getAll(context: NSManagedObjectContext,
-                        sortDescriptors: [NSSortDescriptor]? = nil,
-                        predicate: NSPredicate? = nil) -> [T]
+                       sortDescriptors: [NSSortDescriptor]? = nil,
+                       predicate: NSPredicate? = nil) -> [T]
     {
-
+        
         guard let name = T.entity().name else {
             return []
         }
-    
+        
         let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: name)
         fetchRequest.sortDescriptors = sortDescriptors
         fetchRequest.predicate = predicate
-
+        
         return (try? context.fetch(fetchRequest) as? [T]) ?? []
     }
-
+    
     static func delete(context: NSManagedObjectContext,
-                        sortDescriptors: [NSSortDescriptor]? = nil,
-                        predicate: NSPredicate? = nil) throws
+                       sortDescriptors: [NSSortDescriptor]? = nil,
+                       predicate: NSPredicate? = nil) throws
     {
-
-        getAll(context: context).forEach { context.delete($0) }
-    }
-
-    static func add(context: NSManagedObjectContext,
-                       propertiesToUpdate: [AnyHashable: Any]) throws
-    {
-        do {
-            try update(context: context,
-                        propertiesToUpdate: propertiesToUpdate)
-        } catch {
-            throw CoreDataError.failedToAddObject
-        }
-    }
-
-    private static func update(context: NSManagedObjectContext,
-                                    propertiesToUpdate: [AnyHashable: Any]) throws
-    {
-
         guard let name = T.entity().name else {
             throw CoreDataError.failedToSaveObject
         }
-
-        let request = NSBatchUpdateRequest(entityName: name)
-
-        request.propertiesToUpdate = propertiesToUpdate
-        request.resultType = .updatedObjectsCountResultType
-
-        guard let _ = try? context.execute(request) else {
-            throw CoreDataError.failedToSaveObject
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: name)
+        
+        fetchRequest.predicate = predicate
+        
+        let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+        
+        guard let _ = try? context.execute(deleteRequest) else {
+            throw CoreDataError.failedToDeleteObject
+        }
+    }
+    
+    static func add(context: NSManagedObjectContext,
+                    properties: [String: Any]) throws
+    {
+        guard let name = T.entity().name else {
+            throw CoreDataError.failedToAddObject
+        }
+        
+        let object = NSEntityDescription.insertNewObject(forEntityName: name, into: context) as? T
+        
+        properties.forEach { property in
+            object?.setValue(property.value, forKey: property.key)
+        }
+    }
+    
+    static func update(context: NSManagedObjectContext,
+                       predicate: NSPredicate,
+                       propertiesToUpdate: [String: Any]) throws
+    {
+        guard let object = getAll(context: context, predicate: predicate).first else {
+            throw CoreDataError.notFound
+        }
+        
+        propertiesToUpdate.forEach { property in
+            object.setValue(property.value, forKey: property.key)
         }
     }
 }
