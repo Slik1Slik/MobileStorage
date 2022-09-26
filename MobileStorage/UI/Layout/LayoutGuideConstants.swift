05d6ca59732b1771b676113ece123cd650259d18
Slik <chesto.newsc@ya.ru>
2022-09-26 17:15:14 +0300

    Add layout constants struct

---
+++ MobileStorage/UI/Layout/LayoutGuideConstants.swift
@@ -0,0 +1,22 @@
+//
+//  LayoutGuideConstants.swift
+//  MobileStorage
+//
+//  Created by Slik on 26.09.2022.
+//
+
+import UIKit
+
+struct LayoutGuideConstants {
+    
+    static let screenRect = UIScreen.main.bounds
+    
+    static let padding: CGFloat = 16
+    
+    static let contentInset: CGFloat = 30
+    static let closeContentInset: CGFloat = 14
+    static let groupedContentInset: CGFloat = 10
+    static let objectContentInset: CGFloat = 8
+    
+    static let cornerRadius: CGFloat = 16
+}
