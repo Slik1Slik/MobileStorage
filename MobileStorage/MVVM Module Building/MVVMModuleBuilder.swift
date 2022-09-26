6f617d37464c904459af1f75ed5425b007ebc279
Slik <chesto.newsc@ya.ru>
2022-09-26 17:11:23 +0300

    Add MVVM-model builder protocol

---
+++ MobileStorage/MVVM Module Building/MVVMModuleBuilder.swift
@@ -0,0 +1,12 @@
+//
+//  MVVMModuleBuilder.swift
+//  MobileStorage
+//
+//  Created by Slik on 21.09.2022.
+//
+
+import UIKit
+
+protocol MVVMModuleBuilder {
+    func build() -> UIViewController
+}
